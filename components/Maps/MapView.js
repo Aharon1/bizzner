5c38aaf28012448b67482f4b0cabe6bfd92019ee
Baksha2008 <baksha2008@gmail.com>
2019-01-31 12:09:53 +0200

    fixed modal cluster/marker open

--- components/Maps/MapView.js
+++ components/Maps/MapView.js
@@ -10,6 +10,7 @@ import {
 import { MarkerItem } from "./MarkerItem";
 import { MapClustering } from "./MapClustering";
 import { Modal } from "./components/Modal";
+import { height } from "react-native-dimension";
 
 const dummyImage = require("../../assets/dummy.jpg");
 
@@ -42,50 +43,66 @@ export default class GoogleMapView extends PureComponent {
           ))}
         </MapClustering>
         {isModalOpen && this.renderMarker()}
-        {isModalClusterOpen && this.renderCluster() }
+        {isModalClusterOpen && this.renderCluster()}
       </View>
     );
   }
-  renderMarker = () =>{
-    const { eventData } = this.state
+  renderMarker = () => {
+    const { eventData } = this.state;
     return (
       <Modal
-            eventData={eventData}
-            goToEvent={() => this.goToEvent(eventData.id)}
-            onCloseModal={this.onCloseModal}
-            formatDate={this.formatDate}
-            formatAMPM={this.formatAMPM}
-          />
-    )
-  }
-  renderCluster = () =>{
+        eventData={eventData}
+        goToEvent={() => this.goToEvent(eventData.id)}
+        onCloseModal={this.onCloseModal}
+        formatDate={this.formatDate}
+        formatAMPM={this.formatAMPM}
+      />
+    );
+  };
+
+  renderCluster = () => {
     return (
-      <ScrollView
-            horizontal={true}
-            contentContainerStyle={{ alignItems: "flex-end" }}
-          >
-            {this.state.clusterEvents.map(event => {
-              const id = event.marker.props.id
-              const eventData = event.marker.props
-              return (
-                <Modal
-                  key={id}
-                  goToEvent={() => this.goToEvent(id)}
-                  onCloseModal={this.onModalClusterClose}
-                  eventData={eventData}  
-                  formatDate={this.formatDate}
-                  formatAMPM={this.formatAMPM}
-                />
-              );
-            })}
-          </ScrollView>
-    )
-  }
+      <View style={{height: '50%'}}>
+        <ScrollView
+          horizontal={true}
+          contentContainerStyle={{
+            alignItems: "flex-end",
+            alignSelf: "flex-end",
+          }}
+        >
+          {this.state.clusterEvents.map(event => {
+            const id = event.marker.props.id;
+            const eventData = event.marker.props;
+            return (
+              <Modal
+                key={id}
+                goToEvent={() => this.goToEvent(id)}
+                onCloseModal={this.onModalClusterClose}
+                eventData={eventData}
+                formatDate={this.formatDate}
+                formatAMPM={this.formatAMPM}
+              />
+            );
+          })}
+        </ScrollView>
+      </View>
+    );
+  };
   onOpenModal = eventData => {
-    this.setState({
-      isModalOpen: true,
-      eventData
-    });
+    this.setState(
+      {
+        isModalOpen: true,
+        eventData
+      },
+      this.validateClusterOpen
+    );
+  };
+  validateClusterOpen = () => {
+    if (this.state.isModalClusterOpen) {
+      this.setState({
+        isModalClusterOpen: !this.state.isModalClusterOpen
+      });
+    }
   };
   goToEvent = id => {
     const event_id = id ? id : this.state.eventData.event_id;
@@ -141,11 +158,11 @@ export default class GoogleMapView extends PureComponent {
       clusterEvents
     });
   };
-onModalClusterClose = () =>{
-  this.setState({
+  onModalClusterClose = () => {
+    this.setState({
       isModalClusterOpen: false
-  })
-}
+    });
+  };
 }
 
 const styles = StyleSheet.create({
